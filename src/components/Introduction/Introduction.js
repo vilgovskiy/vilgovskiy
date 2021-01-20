550462b6e726b6ced7b439d89194e34f2492e5a0
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2021-01-20 12:04:51 -0500

    Adjust introduction block

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -5,7 +5,7 @@ const Introduction = React.forwardRef((props, ref) => {
     <header ref={ref} id="introduction" className="Introduction">
       <div>
         <h1>Vladislav Il'govskiy</h1>
-        <h2>FULL-STACK DEVELOPER</h2>
+        <h2>CLOUD & FULL-STACK DEVELOPER</h2>
       </div>
     </header>
   );
